be290bf866d8d6b88438fd29afe2510cb71d0a60
bayanfuad <bayanjawawdeh9@gmail.com>
2022-10-17 18:36:33 +0300

    database

--- src/models/index.js
+++ src/models/index.js
@@ -21,7 +21,7 @@ if (process.env.DATABASE_URL) {
 }
 const db = new Sequelize(
     process.env.DATABASE_URL ||
-    'postgres://postgres:1234@127.0.0.1:5432/edu',
+    'postgres://bayan:123456@127.0.0.1:5432/postgres',
     config 
   )
 
