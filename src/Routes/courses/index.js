ee33d78e23793d1cfe94267776aa2691c488df9a
Hassanabdelqader <terawihassan@gmail.com>
2022-10-21 16:05:49 +0000

    fixed

--- src/Routes/courses/index.js
+++ src/Routes/courses/index.js
@@ -1,15 +1,16 @@
 /*eslint-disable */
 const express = require('express');
 const { __getCourses,__deleteCourse,__addCourse,__updateCourse } = require('../../controller/courseCRUD');
+const ___isAuth = require('../../middlewares/__userMiddlewares/__isAuth');
 
 
 
 const router = express.Router();
 
 router.get('/',__getCourses );
-router.delete('/deletecourse/:id',__deleteCourse );
-router.post('/addCourse',__addCourse);
-router.put('/updateCourse/:id',__updateCourse);
+router.delete('/deletecourse/:id',___isAuth,__deleteCourse );
+router.post('/addCourse',___isAuth,__addCourse);
+router.put('/updateCourse/:id',___isAuth,__updateCourse);
 
 
 
